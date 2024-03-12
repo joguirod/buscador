45f10824c578a66f2e99bd18c058b1bd1aaeef1f
Joguirod <joguicane@gmail.com>
2024-03-12 01:06:03 -0300

    Modificações

--- buscador_features.js
+++ buscador_features.js
@@ -1,6 +1,13 @@
 import * as cheerio from 'cheerio'
 import axios from 'axios'
 import fs from 'fs'
+import {Pilha} from "./pilha.js"
+
+let pilha = new Pilha([])
+
+function load_html(html){
+    return cheerio.load(html)
+}
 
 export async function get_html(url){
     try {
@@ -17,7 +24,7 @@ export async function get_html(url){
 }
 
 function set_document_name(html){
-    const $ = cheerio.load(html)
+    const $ = load_html(html)
     const title = $('title').text().toLowerCase()
     let document_name = title.split(" ")
     if(document_name.length > 1){
@@ -26,33 +33,63 @@ function set_document_name(html){
     return document_name
 }
 
+export async function index_page(url, hashtable, urlBase){
+    if(verify_already_indexed(url, hashtable)){
+        hashtable[url].push(pilha.top())
+        return;
+    }
+    hashtable[url] = []
+    if(pilha.size() > 0){
+        hashtable[url].push(pilha.top())
+    }
+    pilha.push(url)
+    const document = await get_html(urlBase + url)
+    const links = get_links(document)
+    for(let link of links){
+        await index_page(link, hashtable, urlBase)
+    }
+    pilha.pop()
 
-export function verify_self_reference(html_document){
-    const $ = html_document
-    const title = $('title').text().trim()
+}
 
-    const links = $('ul li a');
+function verify_already_indexed(url, ht){
+    return ht.hasOwnProperty(url)
+}
 
-    const has_self_reference = links.filter((index, link) => {
-        const linkText = $(link).text().trim();
-        const titleRegex = new RegExp(title, 'i');
-        return titleRegex.test(linkText);
-    }).length > 0;
+function get_links(document){
+    const $ = load_html(document)
+    const links = []
+        
+    $('a').each((index, element) => {
+        const href = $(element).attr('href');
+        // desconsiderando link vazio e anchor link(link que deve apontar para parte superior da página)
+        if (href && href.trim() !== '' && href !== '#') {
+            links.push(href);
+        }
+    });
 
-    return has_self_reference
+    return links    
+}
+
+export function has_autoreference(link, hashtable){
+    for(const url of hashtable[link]){
+        if(url === link) return true;
+    }
+    return false
 }
 
 export function quantity_of_especific_word(html_document, words){
-    const $ = html_document
+    const $ = load_html(html_document)
     const text_content = $('body').text().toLowerCase()
 
     const word_frequency = {};
     for (const word of words) {
         const regex = new RegExp(word, 'gi'); // 'g' para corresponder globalmente e 'i' para fazer a busca insensível a maiúsculas e minúsculas
-        const matches = text_content.match(regex); // Encontrando todas as ocorrências do termo na página
+        const matches = text_content.match(regex); // encontrando todas as ocorrências do termo na página
 
-        word_frequency[word] = matches ? matches.length : 0; // Armazenando a frequência do termo
+        word_frequency[word] = matches.length; // armazenando a frequência do termo
     }
 
     return word_frequency
 }
+
--- buscador_testes.js
+++ buscador_testes.js
@@ -1,13 +1,24 @@
-import {get_html} 
+import {get_html, index_page, has_autoreference, quantity_of_especific_word} 
 from "./buscador_features.js"
 import {question} from 'readline-sync'
 
 async function main(){
-    const html = await get_html("https://kernel32dev.github.io/hosp-pi/mochileiro.html")
-    console.log(html)
+    // test get_html
+    const html = await get_html("https://kernel32dev.github.io/hosp-pi/blade_runner.html")
+    // console.log(html)
+
+    // test page indexer
+    // const url_inicial = "matrix.html"
+    // const urlBase = "https://kernel32dev.github.io/hosp-pi/"
+
+    // pego os dados da persistência e jogo numa hashtable
+    // let hashtable = {}
+    // await index_page(url_inicial, hashtable, urlBase)
+    // console.log(hashtable)
+    // console.log(has_autoreference("matrix.html", hashtable))
     // verify_self_reference(html) ? console.log("true") : console.log("false")
-    // const words = ['blade', 'runner']
-    // console.log(quantity_of_especific_word(html, words))  
+    const words = ['blade', 'runner']
+    console.log(quantity_of_especific_word(html, words))  
 }
 
 main()
